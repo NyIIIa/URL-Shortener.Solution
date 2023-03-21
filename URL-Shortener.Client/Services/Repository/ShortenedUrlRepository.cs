afe7c7c28eb7210fb49b4b04ac2aa271cdc3b07c
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 18:53:02 +0200

    add implement repositories

---
+++ URL-Shortener.Client/Services/Repository/ShortenedUrlRepository.cs
@@ -0,0 +1,40 @@
+﻿using URL_Shortener.Client.Data;
+using URL_Shortener.Client.Interfaces.Repository;
+using URL_Shortener.Client.Models.Entities;
+
+namespace URL_Shortener.Client.Services.Repository;
+
+public class ShortenedUrlRepository : IShortenedUrlRepository
+{
+    private readonly UrlShortenedDbContext _dbContext;
+
+    public ShortenedUrlRepository(UrlShortenedDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public void Add(ShortenedUrl entity)
+    {
+        var isOriginalUrlExist = _dbContext.ShortenedUrls
+            .Any(su => su.OriginalUrl == entity.OriginalUrl);
+        
+        if (isOriginalUrlExist)
+       {
+           throw new Exception("The original url already exists");
+       }
+
+       _dbContext.ShortenedUrls.Add(entity);
+       _dbContext.SaveChanges();
+    }
+
+    public void Delete(ShortenedUrl shortenedUrl)
+    {
+        _dbContext.ShortenedUrls.Remove(shortenedUrl);
+        _dbContext.SaveChanges();
+    }
+
+    public IEnumerable<ShortenedUrl> GetAll()
+    {
+        return _dbContext.ShortenedUrls.ToList();
+    }
+}
---
+++ URL-Shortener.Client/Services/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using URL_Shortener.Client.Data;
+using URL_Shortener.Client.Interfaces.Repository;
+using URL_Shortener.Client.Models.Entities;
+
+namespace URL_Shortener.Client.Services.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UrlShortenedDbContext _dbContext;
+
+    public UserRepository(UrlShortenedDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public void Add(User entity)
+    {
+        var isUserExist = _dbContext.Users.Any(u => u.Login == entity.Login);
+        if (isUserExist)
+        {
+            throw new Exception("The user with such login already exists");
+        }
+
+        _dbContext.Users.Add(entity);
+        _dbContext.SaveChanges();
+    }
+}
