de9ec193d0fa8851f06cf01d9561e9a503fd1a89
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 18:56:08 +0200

    register db context

--- URL-Shortener.Client/Extensions/DependencyInjection.cs
+++ URL-Shortener.Client/Extensions/DependencyInjection.cs
@@ -1,13 +1,22 @@
-﻿using URL_Shortener.Client.Interfaces.Authentication;
+﻿using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Client.Data;
+using URL_Shortener.Client.Interfaces.Authentication;
 using URL_Shortener.Client.Services.Authentication;
 
 namespace URL_Shortener.Client.Extensions;
 
 public static class DependencyInjection
 {
-    public static void AddInfrastructure(IServiceCollection serviceCollection)
+    public static void AddInfrastructure(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
         serviceCollection.AddScoped<IJwtTokenService, JwtTokenService>();
+    }
+
+    public static void AddDbContext(this IServiceCollection serviceCollection,
+                                    ConfigurationManager configurationManager)
+    {
+        serviceCollection.AddDbContext<UrlShortenedDbContext>(options => 
+            options.UseSqlServer(configurationManager.GetConnectionString("DefaultConnection")));
     }
 }
