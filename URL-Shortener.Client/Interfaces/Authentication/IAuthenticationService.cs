b78c30f1160f40f0deb01f404ad18d91c57f2a3d
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 16:15:21 +0200

    add auth interfaces

---
+++ URL-Shortener.Client/Interfaces/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using URL_Shortener.Client.Models.Authentication;
+using URL_Shortener.Client.Models.DTOs.Authentication;
+
+namespace URL_Shortener.Client.Interfaces.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthResult Register(RegisterRequestDto registerRequest);
+    AuthResult Login(LoginRequestDto loginRequest);
+}
