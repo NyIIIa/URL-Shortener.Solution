b2d6b8eaa83d20ff157b32952b4f951e495a403f
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 16:14:15 +0200

    add base implement auth models

---
+++ URL-Shortener.Client/Models/Authentication/AuthResult.cs
@@ -0,0 +1,6 @@
+﻿namespace URL_Shortener.Client.Models.Authentication;
+
+public class AuthResult
+{
+    public string? Token { get; set; }
+}
---
+++ URL-Shortener.Client/Models/Authentication/JwtSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace URL_Shortener.Client.Models.Authentication;
+
+public class JwtSettings
+{
+    public string? Secret { get; set; }
+    public int ExpiryDays { get; set; }
+}
