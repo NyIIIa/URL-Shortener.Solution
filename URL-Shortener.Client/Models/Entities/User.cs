30425ce0fc9e68e125e163e6e3e4f72f49e55a49
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 18:34:01 +0200

    add db context with entities

---
+++ URL-Shortener.Client/Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace URL_Shortener.Client.Models.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Login { get; set; }
+    public string? PasswordHash { get; set; }
+    public Role? Role { get; set; }
+    public IEnumerable<ShortenedUrl>? ShortenedUrls { get; set; }
+}
