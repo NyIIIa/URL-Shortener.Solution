19b9d7d3311caa5de4f5720447e113d0f0416cb1
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 16:16:19 +0200

    add auth controller

---
+++ URL-Shortener.Client/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using URL_Shortener.Client.Models.DTOs.Authentication;
+
+namespace URL_Shortener.Client.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    public IActionResult Register(RegisterRequestDto registerRequest)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IActionResult Login(LoginRequestDto loginRequest)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IActionResult LoginView()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IActionResult RegisterView()
+    {
+        throw new NotImplementedException();
+    }
+}
