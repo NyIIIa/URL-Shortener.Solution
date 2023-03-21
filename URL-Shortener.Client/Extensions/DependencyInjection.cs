7d5488b4221bf97b2ead07bef68b6b1a3c8c1ed7
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 16:17:28 +0200

    register auth services

---
+++ URL-Shortener.Client/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using URL_Shortener.Client.Interfaces.Authentication;
+using URL_Shortener.Client.Services.Authentication;
+
+namespace URL_Shortener.Client.Extensions;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
+        serviceCollection.AddScoped<IJwtTokenService, JwtTokenService>();
+    }
+}
