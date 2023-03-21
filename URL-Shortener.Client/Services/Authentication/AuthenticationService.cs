237040e7283f72485062dd93180e21014bd40b39
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-21 16:15:45 +0200

    add auth services

---
+++ URL-Shortener.Client/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using URL_Shortener.Client.Interfaces.Authentication;
+using URL_Shortener.Client.Models.Authentication;
+using URL_Shortener.Client.Models.DTOs.Authentication;
+
+namespace URL_Shortener.Client.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IJwtTokenService _jwtTokenService;
+
+    public AuthenticationService(IJwtTokenService jwtTokenService)
+    {
+        _jwtTokenService = jwtTokenService;
+    }
+    
+    public AuthResult Register(RegisterRequestDto registerRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AuthResult Login(LoginRequestDto loginRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ URL-Shortener.Client/Services/Authentication/JwtTokenService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using URL_Shortener.Client.Interfaces.Authentication;
+using URL_Shortener.Client.Models.Authentication;
+
+namespace URL_Shortener.Client.Services.Authentication;
+
+public class JwtTokenService : IJwtTokenService
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenService(IOptions<JwtSettings> jwtOptions)
+    {
+        _jwtSettings = jwtOptions.Value;
+    }
+    
+    public string GenerateToken(string login, string role)
+    {
+        throw new NotImplementedException();
+    }
+}
