bbbd91fd89fed92db4e4447a2aca6a12f33b0cd2
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-22 09:32:20 +0200

    add implement jwt token service

--- URL-Shortener.Client/Services/Authentication/JwtTokenService.cs
+++ URL-Shortener.Client/Services/Authentication/JwtTokenService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using URL_Shortener.Client.Interfaces.Authentication;
 using URL_Shortener.Client.Models.Authentication;
 
@@ -15,6 +19,27 @@ public class JwtTokenService : IJwtTokenService
     
     public string GenerateToken(string login, string role)
     {
-        throw new NotImplementedException();
+        var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.UTF8.GetBytes(_jwtSettings.Secret);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim("Login", login),
+                new Claim(ClaimTypes.Role,role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString())
+            }),
+
+            Expires = DateTime.Now.AddDays(_jwtSettings.ExpiryDays),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+        };
+
+        var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = jwtTokenHandler.WriteToken(token);
+
+        return jwtToken;
     }
 }
