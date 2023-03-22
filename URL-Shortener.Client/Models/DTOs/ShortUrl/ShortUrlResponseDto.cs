653a92b7deedddd2efee79d0500d17751dd46776
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-22 19:58:28 +0200

    add short url response dto

---
+++ URL-Shortener.Client/Models/DTOs/ShortUrl/ShortUrlResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace URL_Shortener.Client.Models.DTOs.ShortUrl;
+
+public class ShortUrlResponseDto
+{
+    public int Id { get; set; }
+    public string? OriginalUrl { get; set; }
+    public string? ShortUrl { get; set; }
+}
