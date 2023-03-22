7d74a4238bf468b3220d5206d1201add3dce3463
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-22 19:57:09 +0200

    add short url service

---
+++ URL-Shortener.Client/Services/ShortUrl/ShortUrlService.cs
@@ -0,0 +1,23 @@
+﻿using URL_Shortener.Client.Interfaces.ShortUrl;
+
+namespace URL_Shortener.Client.Services.ShortUrl;
+
+public class ShortUrlService : IShortUrlService
+{
+    public string ShortUrl(string longUrl)
+    {
+        //Validate the input url
+        if (!Uri.TryCreate(longUrl, UriKind.Absolute, out var inputUrl))
+        {
+            throw new Exception("Invalid url has been provided");
+        }
+        
+        //Create a short version of the input url
+        var random = new Random();
+        const string symbols = "HqJ01z53b7ISOpOVoQhE";
+        var randomStr = new string(Enumerable.Repeat(symbols, 8)
+            .Select(x => x[random.Next(x.Length)]).ToArray());
+        var shortUrl = $"{inputUrl.Host}/{randomStr}";
+        return shortUrl;
+    }
+}
