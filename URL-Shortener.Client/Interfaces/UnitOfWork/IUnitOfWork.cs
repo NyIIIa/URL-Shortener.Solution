0a8b7955e867d90256ff97eaefcc8f2568277571
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-22 09:13:15 +0200

    add UnitOfWork

---
+++ URL-Shortener.Client/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using URL_Shortener.Client.Data;
+using URL_Shortener.Client.Interfaces.Repository;
+
+namespace URL_Shortener.Client.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    UrlShortenedDbContext UrlShortenedDbContext { get; }
+    IUserRepository UserRepository { get; }
+    IShortenedUrlRepository ShortenedUrlRepository { get; }
+    void SaveChanges();
+}
---
+++ URL-Shortener.Client/Services/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using URL_Shortener.Client.Data;
+using URL_Shortener.Client.Interfaces.Repository;
+using URL_Shortener.Client.Interfaces.UnitOfWork;
+using URL_Shortener.Client.Services.Repository;
+
+namespace URL_Shortener.Client.Services.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    public UrlShortenedDbContext UrlShortenedDbContext { get; }
+    public IUserRepository UserRepository { get; }
+    public IShortenedUrlRepository ShortenedUrlRepository { get; }
+
+    public UnitOfWork(UrlShortenedDbContext dbContext)
+    {
+        UrlShortenedDbContext = dbContext;
+        UserRepository = new UserRepository(dbContext);
+        ShortenedUrlRepository = new ShortenedUrlRepository(dbContext);
+    }
+    
+    public void SaveChanges()
+    {
+        UrlShortenedDbContext.SaveChanges();
+    }
+}
