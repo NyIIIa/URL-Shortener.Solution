e9c81b322180f074dd247354ef696af7c6bf8905
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-22 09:18:25 +0200

    register UnitOfWork & JwtSettings

--- URL-Shortener.Client/Extensions/DependencyInjection.cs
+++ URL-Shortener.Client/Extensions/DependencyInjection.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using URL_Shortener.Client.Data;
 using URL_Shortener.Client.Interfaces.Authentication;
+using URL_Shortener.Client.Interfaces.UnitOfWork;
+using URL_Shortener.Client.Models.Authentication;
 using URL_Shortener.Client.Services.Authentication;
+using URL_Shortener.Client.Services.UnitOfWork;
 
 namespace URL_Shortener.Client.Extensions;
 
 public static class DependencyInjection
 {
-    public static void AddInfrastructure(this IServiceCollection serviceCollection)
+    public static void AddInfrastructure(this IServiceCollection serviceCollection, 
+                                        ConfigurationManager configurationManager)
     {
         serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
         serviceCollection.AddScoped<IJwtTokenService, JwtTokenService>();
+        serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        serviceCollection.Configure<JwtSettings>(configurationManager.GetSection("JwtSettings"));
     }
 
     public static void AddDbContext(this IServiceCollection serviceCollection,
--- URL-Shortener.Client/Program.cs
+++ URL-Shortener.Client/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddDbContext(builder.Configuration);
 
 var app = builder.Build();
