351e42966a94ace0c2ed7825c2ffb15b1af78f00
NyIIIa <nedviga.ivan2003@gmail.com>
2023-03-23 08:41:39 +0200

    add updated migration

---
+++ URL-Shortener.Client/Migrations/20230323060925_AddUrlShortAlgorithm.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URL_Shortener.Client.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUrlShortAlgorithm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UrlShortAlgorithms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UrlShortAlgorithms", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UrlShortAlgorithms",
+                columns: new[] { "Id", "Description" },
+                values: new object[] { 1, "" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UrlShortAlgorithms");
+        }
+    }
+}
